940956afc8bc70844741186dfacd986d732e28b5
jflorek <jflorek2@gmail.com>
2017-10-25 16:20:28 -0600

    Added Order class, made OrderLine constructor protected so objects of it can only exist in context of Order object

--- src/OrderApplication.java
+++ src/OrderApplication.java
@@ -1,11 +1,12 @@
 public class OrderApplication {
 
 	public static void main(String[] args) {
+		Order order = new Order();
 		System.out.println("OrderApplication");
 		Product product = new Product("name", 2.3, 2);
 		System.out.println(product.getName());
 		Customer customer = new Customer("name", "address", 1.2, 3.4);
-		OrderLine line = new OrderLine(product, 2);
+		OrderLine line = order.addOrderLine(product, 23);
 		System.out.println(line.calculatePrice());
 	}
 
