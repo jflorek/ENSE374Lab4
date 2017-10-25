ee65ef222cc2f0a261f88ffb10ec2a07e6eb38b2
jflorek <jflorek2@gmail.com>
2017-10-25 15:59:24 -0600

    Added Product class with constructor & getters

--- src/OrderApplication.java
+++ src/OrderApplication.java
@@ -2,7 +2,8 @@ public class OrderApplication {
 
 	public static void main(String[] args) {
 		System.out.println("OrderApplication");
-
+		Product product = new Product("name", 2.3, 2);
+		System.out.println(product.getName());
 	}
 
 }
