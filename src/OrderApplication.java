94264da4dc3e116bafc68bb65a7eb61efd97ff6e
jflorek <jflorek2@gmail.com>
2017-10-25 16:23:55 -0600

    Added Customer, Date, and Id fields to Order (& updated constructor appropriately)

--- src/OrderApplication.java
+++ src/OrderApplication.java
@@ -1,11 +1,13 @@
+import java.util.Date;
+
 public class OrderApplication {
 
 	public static void main(String[] args) {
-		Order order = new Order();
+		Customer customer = new Customer("name", "address", 1.2, 3.4);
+		Order order = new Order(customer, 23, new Date());
 		System.out.println("OrderApplication");
 		Product product = new Product("name", 2.3, 2);
 		System.out.println(product.getName());
-		Customer customer = new Customer("name", "address", 1.2, 3.4);
 		OrderLine line = order.addOrderLine(product, 23);
 		System.out.println(line.calculatePrice());
 	}
