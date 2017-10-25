7988d0ffd023f26938c3a75fe1c5c5ecfc2b052a
jflorek <jflorek2@gmail.com>
2017-10-25 16:06:52 -0600

    Added Customer class

--- src/OrderApplication.java
+++ src/OrderApplication.java
@@ -4,6 +4,7 @@ public class OrderApplication {
 		System.out.println("OrderApplication");
 		Product product = new Product("name", 2.3, 2);
 		System.out.println(product.getName());
+		Customer customer = new Customer("name", "address", 1.2, 3.4);
 	}
 
 }
